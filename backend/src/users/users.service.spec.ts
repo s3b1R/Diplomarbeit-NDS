38278291c7741985361f4b8d5a2a53faaf9b7a7e
Sebastian <sebastian.rueegg@bluewin.ch>
2021-02-27 21:52:37 +0100

    users.service unitTests

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
+import { Users } from './users.entity';
+
+export const mockRepository = jest.fn(() => ({
+  find: () =>
+    new Promise((resolve) => {
+      resolve([{ id: 1, name: 'Hans Muster' }]);
+    }),
+}));
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(Users),
+          useClass: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -14,5 +29,13 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('Can find all users', async () => {
+    const usersFromDB: Users[] = await service.findAll();
+
+    expect(Array.isArray(usersFromDB)).toBeTruthy();
+    expect(usersFromDB[0].id).toBe(1);
+    expect(usersFromDB[0].name).toBe('Hans Muster');
   });
 });
