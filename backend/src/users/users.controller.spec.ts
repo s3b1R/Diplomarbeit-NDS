c146bdf7e039981080fb4993b3a98fb461bcdf80
Sebastian <sebastian.rueegg@bluewin.ch>
2021-02-28 14:25:03 +0100

    unittest controller

--- backend/src/users/users.controller.spec.ts
+++ backend/src/users/users.controller.spec.ts
@@ -1,14 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+jest.mock('../users/users.service.ts');
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService],
     }).compile();
 
+    service = module.get<UsersService>(UsersService);
     controller = module.get<UsersController>(UsersController);
   });
 
