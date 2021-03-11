0a6ab5b33e1c563c022bd41bc1ecc73203492edf
Sebastian <sebastian.rueegg@bluewin.ch>
2021-03-11 22:52:35 +0100

    Material Design & Datepicker

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CapacityComponent } from './capacity/capacity.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DateFnsModule } from 'ngx-date-fns';
+import { NgxMatDateFnsDateModule} from 'ngx-mat-datefns-date-adapter';
+import {MAT_DATE_LOCALE} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
@@ -12,9 +20,15 @@ import { CapacityComponent } from './capacity/capacity.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DateFnsModule,
+    NgxMatDateFnsDateModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
-  providers: [],
+  providers: [{provide: MAT_DATE_LOCALE, useValue: 'de'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/capacity/capacity.component.ts
+++ frontend/src/app/capacity/capacity.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-capacity',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CapacityComponent implements OnInit {
 
+  date  =  new  FormControl(new  Date());
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
