57ab494318f63902eb3bb6e0cb890b929a7a6711
Sebastian <sebastian.rueegg@bluewin.ch>
2021-03-13 20:20:38 +0100

    Put capacity in table

--- frontend/src/app/capacity/capacity.component.ts
+++ frontend/src/app/capacity/capacity.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {eachDayOfInterval, lastDayOfMonth, startOfMonth} from 'date-fns';
+import {eachDayOfInterval, lastDayOfMonth, startOfMonth, format} from 'date-fns';
 import {MatDatepickerInputEvent} from '@angular/material/datepicker';
 
 
@@ -11,8 +11,16 @@ import {MatDatepickerInputEvent} from '@angular/material/datepicker';
 export class CapacityComponent implements OnInit {
 
   constructor() { }
-
+  dayOutOfTable: any;
   interval: any = [];
+  users: any = [{id: 1, name: 'Hans Muster'}, {id: 2, name: 'Sebastian Rüegg'}, {id: 3, name: 'Peter Lustig'},
+    {id: 4, name: 'Peter Müller'}, {id: 7, name: 'Greg Müller'}];
+  capacity: any = [
+    {id: 1, capa: '0.8', date: '2021-03-14', user: {id: 1, name: 'Hans Muster'}},
+    {id: 2, capa: '0.9', date: '2021-03-15', user: {id: 2, name: 'Sebastian Rüegg'}},
+    {id: 3, capa: '1.0', date: '2021-03-16', user: {id: 2, name: 'Sebastian Rüegg'}},
+    {id: 4, capa: '1.0', date: '2021-03-14', user: {id: 2, name: 'Peter Müller'}}
+    ];
 
   ngOnInit(): void {
     this.updateInterval(new Date());
@@ -28,6 +36,11 @@ export class CapacityComponent implements OnInit {
       end: lastDayOfMonth(new Date(date))
     });
   }
+
+checkDate(dayOfMonth: any, capaDate: any): boolean{
+    this.dayOutOfTable = format(dayOfMonth, 'yyyy-MM-dd');
+    return this.dayOutOfTable === capaDate;
+}
 
 
 }
