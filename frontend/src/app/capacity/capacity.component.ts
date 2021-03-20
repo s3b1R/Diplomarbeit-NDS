32cc4010def468f5d6279da04c6a434585da0d3d
Sebastian <sebastian.rueegg@bluewin.ch>
2021-03-20 12:14:02 +0100

    First part of fillCapa function

--- frontend/src/app/capacity/capacity.component.ts
+++ frontend/src/app/capacity/capacity.component.ts
@@ -4,6 +4,7 @@ import {MatDatepickerInputEvent} from '@angular/material/datepicker';
 import {ApiService} from '../services/api.service';
 import { Capacity } from '../models/capacity.model';
 import { User } from '../models/user.model';
+import {forkJoin} from 'rxjs';
 
 
 @Component({
@@ -14,27 +15,27 @@ import { User } from '../models/user.model';
 export class CapacityComponent implements OnInit {
 
   constructor(private capacityService: ApiService) { }
-  dayOutOfTable: any;
   interval: any = [];
-  users: User[];
-  capacity: Capacity[];
-  capaView = new Map();
+  userList: User[];
+  rawCapacity: Capacity[];
+  rawCapacityPerUser = new Map();
+  allCapacities: Capacity[];
 
   ngOnInit(): void {
     this.updateInterval(new Date());
-    this.getAllCapacities();
-    this.getAllUsers();
+    this.getCapacitiesAndUsers();
   }
 
-  getAllCapacities(): void {
-   this.capacityService.getAllCapacities().subscribe(capacities => {
-     this.capacity = capacities;
-     this.createCapaView(this.capacity);
-   });
-  }
-
-  getAllUsers(): void {
-    this.capacityService.getAllUsers().subscribe( users => this.users = users);
+  getCapacitiesAndUsers(): void {
+    forkJoin([ this.capacityService.getAllUsers(),
+      this.capacityService.getAllCapacities() ])
+      .subscribe(results => {
+        this.userList = results[0];
+        this.rawCapacity = results[1];
+        this.createCapaView(this.rawCapacity);
+        this.allCapacities = this.fillCapa(this.rawCapacityPerUser, this.userList);
+        console.log(this.allCapacities);
+    });
   }
 
   dateChange(event: MatDatepickerInputEvent<any>): void {
@@ -49,12 +50,12 @@ export class CapacityComponent implements OnInit {
   }
 
   checkDate(dayOfMonth: any, capaDate: any): boolean{
-    this.dayOutOfTable = format(dayOfMonth, 'yyyy-MM-dd');
-    return this.dayOutOfTable === capaDate;
+    const dayOutOfTable = format(dayOfMonth, 'yyyy-MM-dd');
+    return dayOutOfTable === capaDate;
   }
 
-  logCapacity(): void {
-    console.log(this.capaView);
+  sayHello(): void {
+    alert('Hello!');
   }
 
   createCapaView(givenCapa: Capacity[]): void {
@@ -84,12 +85,76 @@ export class CapacityComponent implements OnInit {
 
         userCapas.push(currentEntry);
         userMap.set(user, userCapas);
-        this.capaView.set(currentEntry.user.name, userMap);
+        this.rawCapacityPerUser.set(currentEntry.user.name, userMap);
         userMap = new Map();
       }
     }
+  }
+
+  fillCapa(rawCapacityMap: Map<string, Map<string, Array<Capacity>>>, userList: User[]): Capacity[]{
+    // console.log('Format log: ' + format(this.interval[0], 'yyyy-MM-dd'));
+    // console.log('userArray ' + userList.length);
+    // console.log('capaView ' + rawCapacityMap.size);
+    const userMap = new Map();
+    const capaForUserExists = [];
+    const allCapacities = [];
+
+    for (let userIndex = 0; userIndex < userList.length; userIndex++){
+      const userName = userList[userIndex].name;
+      // console.log('userArrayLoop: ' + user.name);
+
+      for (let intervalIndex = 0; intervalIndex < this.interval.length; intervalIndex++) {
+        const date = format(this.interval[intervalIndex], 'yyyy-MM-dd');
+        // console.log('intervalLoop: ' + format(this.interval[index], 'yyyy-MM-dd'));
+
+        for (const [key, capacityMap] of rawCapacityMap.entries()){
+          // console.log(user.name + ' yes');
+          if (rawCapacityMap.has(userName)){
+
+            if (key === userName){
+              // console.log('capaView Loop: ' + key, capacityMap);
+              const capaArray = capacityMap.get(userName);
+              const dateArray = [];
+              capaArray.forEach( (capacity) => {
+                dateArray.push(capacity.date);
+              });
+
+              // console.log(dateArray.includes(date));
+
+              if (dateArray.includes(date)){
+                for (let capaIndex = 0; capaIndex < capaArray.length; capaIndex++){
+                  // console.log(capaArray[index].date);
+
+                  if (format(this.interval[intervalIndex], 'yyyy-MM-dd') === capaArray[capaIndex].date){
+                    console.log(capaArray[capaIndex] instanceof Capacity );
+                    allCapacities.push(capaArray[capaIndex]);
+                  }
+                }
+              } else {
+                // dumy für datum schreiben
+                console.log(userName + ' ' + format(this.interval[intervalIndex], 'yyyy-MM-dd'));
+              }
 
 
+            }
+          }
+          else {
+            // console.log(user.name + ' no');
+            // dummy für datum schreiben
+            if (!capaForUserExists.includes(userName + date)){
+              capaForUserExists.push(userName + date);
+              console.log(userName + ' ' + format(this.interval[intervalIndex], 'yyyy-MM-dd'));
+            }
+
+          }
+        }
+
+      }
+
+    }
+    console.log(allCapacities);
+
+    return allCapacities;
   }
 
 }
