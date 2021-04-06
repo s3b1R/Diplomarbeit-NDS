973e77100c1efb6ea660bce02675e08928a7a33a
Sebastian <sebastian.rueegg@bluewin.ch>
2021-04-06 18:22:26 +0200

    routing occupancy module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import {CapacityComponent} from './capacity/capacity.component';
 import {CapacityMassmutationComponent} from './capacity-massmutation/capacity-massmutation.component';
 import {UserComponent} from './user/user.component';
-import {WorkloadComponent} from "./workload/workload.component";
+import {WorkloadComponent} from './workload/workload.component';
+import {OccupancyComponent} from './occupancy/occupancy.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/capaview', pathMatch: 'full'},
   {path: 'capaview', component: CapacityComponent},
   {path: 'massmutation', component: CapacityMassmutationComponent},
   {path: 'user', component: UserComponent},
+  {path: 'occupancy', component: OccupancyComponent},
   {path: 'workload', component: WorkloadComponent}
 ];
 
