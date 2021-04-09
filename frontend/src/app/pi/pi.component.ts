14aaeef9040a4724ef8a7574bd5ab24f06a5f4a0
Sebastian <sebastian.rueegg@bluewin.ch>
2021-04-09 19:04:38 +0200

    create, update and delete PI

--- frontend/src/app/pi/pi.component.ts
+++ frontend/src/app/pi/pi.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Pi } from '../shared/models/pi.model';
+import { format, parse } from 'date-fns';
+import { ApiService } from '../shared/services/api.service';
+import { ConfirmationDialogComponent } from '../shared/confirmation-dialog/confirmation-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-pi',
@@ -7,9 +13,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PiComponent implements OnInit {
 
-  constructor() { }
+  piList: Pi[];
+  newPi = new FormGroup({
+    shortName: new FormControl(),
+    startDate: new FormControl(),
+    endDate: new FormControl(),
+    sprintCounts: new FormControl()
+  });
+  piControl = new FormControl();
+
+
+  constructor(private apiService: ApiService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.loadPiList();
   }
 
+  submitNewPi(): void {
+    this.apiService.newPi(
+      this.newPi.value.shortName,
+      format(this.newPi.value.startDate, 'yyyy-MM-dd'),
+      format(this.newPi.value.endDate, 'yyyy-MM-dd'),
+      this.newPi.value.sprintCounts).subscribe();
+    this.loadPiList();
+  }
+
+  updatePi(newPiShortName: string, newStartDate: string, newEndDate: string, newSprintCount: string): void {
+    this.apiService.updatePi(
+      this.piControl.value.id,
+      newPiShortName,
+      format(parse(newStartDate, 'dd/MM/yyyy', new Date()), 'yyyy-MM-dd'),
+      format(parse(newEndDate, 'dd/MM/yyyy', new Date()), 'yyyy-MM-dd'),
+      Number(newSprintCount)
+    );
+    this.piControl.reset();
+    this.loadPiList();
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: `PI ${this.piControl.value.piShortname} löschen?`
+    });
+
+    dialogRef.afterClosed().subscribe( result => {
+      if (result) {
+        this.deletePi();
+      }
+    });
+  }
+
+  deletePi(): void {
+    this.apiService.deletePi(this.piControl.value.id);
+    this.piControl.reset();
+    this.loadPiList();
+  }
+
+  private loadPiList(): void {
+    this.waitAMoment().then(() => {
+      this.apiService.getPiData().subscribe(results => {
+        this.piList = results;
+      });
+    });
+  }
+
+  async waitAMoment(): Promise<void> {
+    await this.delay(500);
+  }
+
+  delay(ms: number): any {
+    return new Promise( resolve => setTimeout(resolve, ms));
+  }
 }
