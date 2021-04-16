f6623f6de1edd3c0b03e8b4610c2f1789bd7ae92
Sebastian <sebastian.rueegg@bluewin.ch>
2021-04-16 15:16:04 +0200

    Comparison

--- frontend/src/app/comparison/comparison.component.spec.ts
+++ frontend/src/app/comparison/comparison.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ApiService } from '../shared/services/api.service';
 import { ComparisonComponent } from './comparison.component';
 
 describe('ComparisonComponent', () => {
@@ -8,7 +9,7 @@ describe('ComparisonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ComparisonComponent ]
+      declarations: [ ComparisonComponent ], imports: [HttpClientTestingModule], providers: [ApiService]
     })
     .compileComponents();
   });
--- frontend/src/app/comparison/comparison.component.ts
+++ frontend/src/app/comparison/comparison.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/models/user.model';
+import { Pi } from '../shared/models/pi.model';
+import { ApiService } from '../shared/services/api.service';
+
 
 @Component({
   selector: 'app-comparison',
@@ -6,10 +10,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comparison.component.css']
 })
 export class ComparisonComponent implements OnInit {
+  piList: Pi[];
+  userList: User[];
+  shownPi: Pi;
+  sprints: number[];
+  piName: number;
+  sprintStarts = [];
+  sprintEnds = [];
 
-  constructor() { }
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.apiService.getPiData().subscribe(result => {
+      this.piList = result;
+      this.shownPi = result[0];
+      this.sprints = Array(this.shownPi.sprintCounts).fill(1).map((x, i) => i + 1);
+      this.piName = this.shownPi.piShortname;
+      this.getSprintStartandEndDates(this.shownPi);
+
+    });
+
+    this.apiService.getAllUsers().subscribe(result => {
+      this.userList = result;
+    });
   }
+
+  getSprintCapa(userId: number, sprintStart: string, sprintEnd: string): number {
+    let capa = 0;
+    this.apiService.getCapacityForUserInSprint(userId, sprintStart, sprintEnd)
+      .subscribe(data => capa = data);
+    return capa;
+  }
+
+  getSprintWorkload(name: string, sprint: string): number {
+    let workload = 0;
+    this.apiService.getWorkloadForUserInSprint(name, sprint)
+      .subscribe(data => workload = data);
+    return workload;
+  }
+
+  getSprintStartandEndDates(pi): void {
+    Object.keys(pi).map(key => {
+
+      if (key.includes('sprint') && key.includes('Start')){
+        this.sprintStarts.push(pi[key]);
+      }
+      if (key.includes('sprint') && key.includes('End')){
+        this.sprintEnds.push(pi[key]);
+      }
+    });
+  }
+
+
+
+
 
 }
