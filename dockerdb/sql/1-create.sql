9652d3565f4b0dc34cf263b593eb05709d90b007
Sebastian <sebastian.rueegg@bluewin.ch>
2021-04-27 08:50:38 +0200

    Update 1-create.sql

--- dockerdb/sql/1-create.sql
+++ dockerdb/sql/1-create.sql
@@ -1,16 +1,7 @@
 DROP USER IF EXISTS 'nestBackend'@'%';
-# DROP USER IF EXISTS 'nestBackend'@'172.17.0.1';
 DROP DATABASE IF EXISTS resourceplanner;
 
-# CREATE USER 'nestBackend'@'localhost' IDENTIFIED BY 'resourceplanner';
-# CREATE DATABASE resourceplanner DEFAULT CHARACTER SET utf8;
-# GRANT ALL PRIVILEGES ON resourceplanner.* TO 'nestBackend'@'localhost';
-# FLUSH PRIVILEGES;
-
+CREATE DATABASE resourceplanner DEFAULT CHARACTER SET utf8;
 CREATE USER 'nestBackend'@'%' IDENTIFIED BY 'resourceplanner';
 GRANT ALL PRIVILEGES ON resourceplanner.* TO 'nestBackend'@'%';
 FLUSH PRIVILEGES;
-
-# CREATE USER 'nestBackend'@'172.0.0.0/255.0.0.0' IDENTIFIED BY 'resourceplanner';
-# GRANT ALL PRIVILEGES ON resourceplanner.* TO 'nestBackend'@'172.0.0.0/255.0.0.0';
-# FLUSH PRIVILEGES;
